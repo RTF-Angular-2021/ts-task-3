2441e17ee7d015f6eb02ece83c415cd4c1cffa17
vnovikov <v.novikov@artsofte.ru>
2021-04-09 20:14:22 +0500

    Добавлены задачи для третьего блока практик по TypeScript

---
+++ src/task_1/index.ts
@@ -0,0 +1,28 @@
+/** Задача 1
+ * 1) Изучить принцип работы паттерна декоратор (https://refactoring.guru/ru/design-patterns/decorator)
+ * 2) Для метода exec класса Calculator реализовать два классических декоратора:
+ * 		2.1) DecorateRu - Оборачивает работу метода exec и выводит
+ * 			 результат в виде строки на русском языке:
+ * 			 результат сложения ${a} + ${b} = ${рассчитанное значение}
+ *		2.2) DecorateEn - Оборачивает работу метода exec и выводит
+ *			 результат в виде строки на английском языке:
+ *			 result of the addition operation ${a} + ${b} = ${рассчитанное значение}
+*/
+
+class Calculator {
+    protected a: number = 0;
+    protected b: number = 0;
+
+    constructor(a: number, b: number) {
+        if (!!a) {
+            this.a = a;
+        }
+        if (!!b) {
+            this.b = b;
+        }
+    }
+	
+    public exec(): string {
+        return (this.a + this.b).toString();
+    }
+}
---
+++ src/task_4/index.ts
@@ -0,0 +1,11 @@
+/** Задача 4
+ * Описать каким должен быть объект X, чтобы метод работал корректно
+*/
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+const x = undefined;
+
+console.log(getProperty(x, "m")); 
