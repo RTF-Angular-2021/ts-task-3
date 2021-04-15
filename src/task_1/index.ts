022b9affb60e3b14328ce087b37d340e7ed975bf
Foxovsky-bit <mihalov.k@mail.ru>
2021-04-15 22:11:39 +0500

    3 задания

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -8,6 +8,18 @@
  *			 результат в виде строки на английском языке:
  *			 result of the addition operation ${a} + ${b} = ${рассчитанное значение}
 */
+function DecorateRu(target:any,method:string,descriptor:PropertyDescriptor) {
+        descriptor.value = function() {
+            return `результат сложения ${this.a} + ${this.b} = ${this.a + this.b}`;
+        }
+}
+
+function DecorateEn(target:any,method:string,descriptor:PropertyDescriptor) {
+        descriptor.value = function() {
+            return `result of the addition operation ${this.a} + ${this.b} = ${this.a + this.b}`;
+        }
+}
+
 
 class Calculator {
     protected a: number = 0;
@@ -22,7 +34,11 @@ class Calculator {
         }
     }
 	
+    @DecorateEn
     public exec(): string {
         return (this.a + this.b).toString();
     }
 }
+
+let calc = new Calculator(100,25);
+console.log(calc.exec());
