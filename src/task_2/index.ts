a5d95a5c29fc2167a57879e0d8e7346ccbbb9443
evgeniy <evgeniy.vm11@gmail.com>
2021-04-17 14:17:39 +0500

    Михайловский Евгений 3 задания

--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -12,3 +12,21 @@ class Example {
 let exampleInstance = new Example();
 exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
 exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
+
+function validateMail(target: Object, propertyKey: string | symbol): any {
+    let email = ""
+    let descriptor: PropertyDescriptor = {
+        get() {
+            return email
+        },
+        set(newEmail: string) {
+            if (newEmail !== "" && !/[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+/.test(newEmail)) {
+                throw 'Invalid email.'
+            }
+            console.log('email valid');
+            email = newEmail;
+        }
+    }
+
+    return descriptor;
+}
