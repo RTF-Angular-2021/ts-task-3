e89912dd758ec6f442e16dd9a13140f4795a5678
diukin9 <petr.diukin@urfu.me>
2021-05-19 16:40:56 +0500

    Дюкин Петр ts3 (2 номера)

--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -1,14 +1,21 @@
-/** Задача 1
- * Реализовать декоратор, используя встроенную поддержку декораторов в TypeScript,
- * который будет реагировать на присвоение в поле email значения.
- * Когда присваивается корректный e-mail в консоль выводится сообщение email valid.
- * Когда присваивается некорректный e-mail возбуждается ошибка.
-*/
+function isValidEmail(target: Object, propertyKey: string): any {
+    let email: string;
+    let descriptor: PropertyDescriptor = {
+        set(newEmail: string) {
+            if (!/[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+/.test(newEmail))
+                throw 'email is invalid'
+            console.log('email is valid');
+            email = newEmail;
+        }
+    }
+    return descriptor;
+}
 
 class Example {
+    @isValidEmail
     public email: string = "";
 }
 
 let exampleInstance = new Example();
-exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
-exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
+exampleInstance.email = "fkkldfjg";
+exampleInstance.email = "misha@mail.ru";
